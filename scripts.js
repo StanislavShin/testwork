da0952f66442a599e1396c8eb3fd97161aa611f5
Stanislav <moonsu8747@gmail.com>
2019-07-08 15:09:20 +0600

    second day
    added slider, forms

---
+++ scripts.js
@@ -0,0 +1,43 @@
+$('.slider_button').on('click', function(e){
+	e.preventDefault();
+
+	var $this = $(this),
+			container = $this.closest('.slider'),
+			list = container.find('.slider_list'),
+			item = list.find('.slider_item'),
+			activeSlide = item.filter('.active'),
+			prevSlide = activeSlide.prev(),
+			nextSlide = activeSlide.next(),
+			firstSlide = item.first(),
+			lastSlide = item.last(),
+			slideOffset = container.offset().left,
+			reqPos = 0;
+
+		if($this.hasClass('slider_btn-next')) {
+			if(nextSlide.length){
+				findReqPos(nextSlide);
+			    addActiveClass(nextSlide);
+			}else{
+				findReqPos(firstSlide);
+				addActiveClass(firstSlide);
+			}
+			
+		}else{
+			if(prevSlide.length){
+				findReqPos(prevSlide);
+				addActiveClass(prevSlide);
+			}else{
+				findReqPos(lastSlide);
+				addActiveClass(lastSlide);
+			}
+			
+		}
+		list.css('left', '-=' + reqPos + 'px');
+
+		function addActiveClass(reqSlide){
+			reqSlide.addClass('active').siblings().removeClass('active');
+		};
+		function findReqPos(slide) {
+			reqPos = slide.offset().left - slideOffset;
+		};
+	});
